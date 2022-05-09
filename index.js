ed6eca21b5f29f5094e2d645784a90561cdf1bc9
UD <johndoe@example.com>
2022-05-09 09:03:36 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+//punto de montaje : /hello
+app.get('/hello', (req, res) => {
+  res.send('Hello World111!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+/*
+var http = require('http');
+
+    http.createServer(function(req, res){
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('Hello World!');
+    }).listen(8080);*/
